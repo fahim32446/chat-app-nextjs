ff2b64c96029744de44a49fadf7d5a559cf97850
Azmir <azmirhasan.az2@gmail.com>
2025-04-26 22:25:22 +0600

    Fix type error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   // ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+  },
   ...compat.config({
     extends: ['next'],
     rules: {
